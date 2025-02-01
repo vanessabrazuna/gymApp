0350c4029d39161c21f335cbe742d66ac93f7d79
vanessabrazuna <brazuna.nessa@gmail.com>
2025-02-01 20:24:04 -0300

    feat: Use cases, tests and authentication controller

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,37 @@
+import bcrypt from "bcryptjs"
+import { User } from "@prisma/client"
+import { UsersRepository } from "src/repositories/users.repository"
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error"
+
+const { compare } = bcrypt
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
